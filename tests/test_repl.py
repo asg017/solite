4573fa380bf50e17264acbcb5c5d32551a5bd10f
Alex Garcia <alexsebastian.garcia@gmail.com>
2025-02-07 22:48:20 -0800

    replace version string

--- tests/test_repl.py
+++ tests/test_repl.py
@@ -1,5 +1,6 @@
+import re
+
 def test_repl(solite_cli, snapshot):
-    assert (
-        solite_cli([], communicate=[b".timer off\nselect 1 + 1;\n"], kill=True).stdout
-        == snapshot
-    )
+    output = solite_cli([], communicate=[b".timer off\nselect 1 + 1;\n"], kill=True).stdout
+    output = re.sub(r"Solite \d+\.\d+\.\d+(-[a-z]+\.\d+)?", "Solite VERSION", output)
+    assert output == snapshot
